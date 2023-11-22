7749be142474e8c8abdda3ee1cb7abdd7742cc58
Bubbelbad <victor.ivarson@hotmail.se>
2023-11-22 14:09:46 +0100

    added classes User and Movie

---
+++ FilmLibraryWPF/Classes/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmLibraryWPF.Classes
+{
+    class Movie
+    {
+        public int Id { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private string Url { get; set; }
+
+        public static int nextId = 1;
+
+        public Movie(string title, string description, string url)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Id = nextId++;
+        }
+    }
+}
---
+++ FilmLibraryWPF/Classes/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace FilmLibraryWPF.Classes
+{
+    class User
+    {
+
+        public int Id { get; set; }
+        private string Name { get; set; }
+        private string LastName { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+
+        public static int nextId = 1;
+
+        public User(string name, string lastName, string email, string password)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+            this.Email = email;
+            this.Password = password;
+            this.Id = nextId++;
+        }
+    }
+}
