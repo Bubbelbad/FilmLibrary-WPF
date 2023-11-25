2b3f8f29b88485cdf910af5316d827cc828cb284
Bubbelbad <victor.ivarson@hotmail.se>
2023-11-25 14:51:24 +0100

    updated on LoginWindow

--- FilmLibraryWPF/MainWindow.xaml.cs
+++ FilmLibraryWPF/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using FilmLibraryWPF.View.UserControls;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,14 +28,7 @@ namespace FilmLibraryWPF
 
         public MainWindow()
         {
-            
-            
-        }
-
-        public void logIn()
-        {
-            LoginWindow loginWindow = new LoginWindow();
-            loginWindow.Visibility = Visibility.Visible;
+            InitializeComponent();
         }
 
     }
