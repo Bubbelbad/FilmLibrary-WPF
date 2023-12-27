e0dbe18464fb238589beb50785ca13d0876cbafc
Bubbelbad <victor.ivarson@hotmail.se>
2023-12-27 12:12:58 +0100

    new close button on profile

--- FilmLibraryWPF/View/UserControls/Profile.xaml.cs
+++ FilmLibraryWPF/View/UserControls/Profile.xaml.cs
@@ -24,5 +24,10 @@ namespace FilmLibraryWPF.View.UserControls
         {
             InitializeComponent();
         }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+        }
     }
 }
